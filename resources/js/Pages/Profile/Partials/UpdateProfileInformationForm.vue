af03c10e16fd42d2f7ff547391e97cbe766fa40d
jkcoderdev <20jackoza06@gmail.com>
2025-01-13 14:54:50 +0100

    Clean up and fix UpdateProfileInformationForm.vue partial

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
@@ -24,12 +24,12 @@ const form = useForm({
 
 <template>
     <section>
-        <header>
+        <header class="text-box">
             <h2>
                 Profile Information
             </h2>
 
-            <p class="desc">
+            <p>
                 Update your account's profile information and email address.
             </p>
         </header>
@@ -44,14 +44,14 @@ const form = useForm({
                 <TextInput
                     id="name"
                     type="text"
-                    class="text-input"
+                    class="input"
                     v-model="form.name"
                     required
                     autofocus
                     autocomplete="name"
                 />
 
-                <InputError class="input-error" :message="form.errors.name" />
+                <InputError class="error" :message="form.errors.name" />
             </div>
 
             <div>
@@ -60,13 +60,13 @@ const form = useForm({
                 <TextInput
                     id="email"
                     type="email"
-                    class="text-input"
+                    class="input"
                     v-model="form.email"
                     required
                     autocomplete="username"
                 />
 
-                <InputError class="input-error" :message="form.errors.email" />
+                <InputError class="error" :message="form.errors.email" />
             </div>
 
             <div v-if="mustVerifyEmail && user.email_verified_at === null">
@@ -90,14 +90,14 @@ const form = useForm({
                 </div>
             </div>
 
-            <div class="btn">
+            <div class="submit-box">
                 <PrimaryButton :disabled="form.processing">Save</PrimaryButton>
 
                 <Transition
-                    enter-active-class="transition ease-in-out"
-                    enter-from-class="opacity-0"
-                    leave-active-class="transition ease-in-out"
-                    leave-to-class="opacity-0"
+                    enter-active-class="transition-enter-active"
+                    enter-from-class="transition-enter-from"
+                    leave-active-class="transition-leave-active"
+                    leave-to-class="transition-leave-to"
                 >
                     <p
                         v-if="form.recentlySuccessful"
@@ -112,97 +112,182 @@ const form = useForm({
 </template>
 
 <style lang="scss" scoped>
-
-    $semi-black: #111;
-    $gray: #333;
-    $ligt-gray: #666;
-    $indigo: #6366f1;
-
-    @mixin smalltext{
-        font-size: 0.875rem;
-        line-height: 1.25rem;
-    }
-
-    @mixin largetext{
-        line-height: 1.75rem;
-        font-weight: 500;
-    }
-
-    header{
-        h2{
-            @include largetext;
-
+section {
+    .text-box {
+        h2 {
             font-size: 1.125rem;
-            color: $gray;
+            line-height: 1.75rem;
+            font-weight: 500;
+            color: #111827;
         }
 
-        .desc{
-            @include smalltext;  
-
-            margin-top: .25rem;
-            color: $ligt-gray;
+        p {
+            margin-top: 0.25rem;
+            font-size: 0.875rem;
+            line-height: 1.25rem;
+            color: #4b5563;
         }
     }
 
-    form{
+    .form {
         margin-top: 1.5rem;
-        margin-bottom: 1.5rem;
 
-        .text-input{
-            margin-top: .25rem;
-            display: block;
+        display: flex;
+        flex-direction: column;
+        gap: 1.5rem;
+
+        .input {
+            margin-top: 0.25rem;
             width: 100%;
+            display: block;
         }
 
-        .input-error{
-            margin-top: .5rem;
+        .error {
+            margin-top: 0.5rem;
         }
 
-        .verify-email{
-            @include smalltext;
-
-            margin-top: .5rem;
-            color: $gray;
-
-            .link{
-                border-radius: 0.375rem;
-                @include smalltext;
-                color: $ligt-gray;
-                text-decoration: underline;
-                
-                &:hover{
-                    color: $semi-black;
-                }
-
-                &:focus{
-                    outline: none;
-                    box-shadow: 0 0 0 2px $indigo, 0 0 0 4px rgba(255, 255, 255, 1);
-                }
-            }
-
-            .message{
-                @include smalltext;
-
-                margin-top: .5rem;
-                font-weight: 500;
-                color: rgb(0, 156, 0);
-            }
-            
-        }
-
-        .btn{
+        .submit-box {
             display: flex;
             align-items: center;
             gap: 1rem;
-            margin-top: 1rem;
 
-            .message{
-                @include smalltext;
+            .email {
+                margin-top: 0.5rem;
 
-                color: $ligt-gray;
+                font-size: 0.875rem;
+                line-height: 1.25rem;
+
+                color: #1f2937;
+
+                .link {
+                    font-size: 0.875rem;
+                    line-height: 1.25rem;
+
+                    border-radius: 0.375rem;
+                    color: #4b5563;
+                    text-decoration: underline;
+
+                    &:hover {
+                        color: #111827;
+                    }
+
+                    &:focus {
+                        box-shadow: 0 0 0 2px #ffffff, 0 0 0 4px #6366f1;
+                    }
+                }
+            }
+
+            .message {
+                margin-top: 0.5rem;
+                font-size: 0.875rem;
+                line-height: 1.25rem;
+                color: #16a34a;
+            }
+
+            .transition-enter-active,
+            .transition-leave-active {
+                transition-property: color;
+                transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+                transition-duration: 0.15s;
+            }
+
+            .transition-enter-from,
+            .transition-leave-to {
+                opacity: 0;
             }
         }
-
-        
     }
+}
+// $semi-black: #111;
+// $gray: #333;
+// $ligt-gray: #666;
+// $indigo: #6366f1;
+
+// @mixin smalltext{
+//     font-size: 0.875rem;
+//     line-height: 1.25rem;
+// }
+
+// @mixin largetext{
+//     line-height: 1.75rem;
+//     font-weight: 500;
+// }
+
+// header{
+//     h2{
+//         @include largetext;
+
+//         font-size: 1.125rem;
+//         color: $gray;
+//     }
+
+//     .desc{
+//         @include smalltext;  
+
+//         margin-top: .25rem;
+//         color: $ligt-gray;
+//     }
+// }
+
+// form{
+//     margin-top: 1.5rem;
+//     margin-bottom: 1.5rem;
+
+//     .text-input{
+//         margin-top: .25rem;
+//         display: block;
+//         width: 100%;
+//     }
+
+//     .input-error{
+//         margin-top: .5rem;
+//     }
+
+//     .verify-email{
+//         @include smalltext;
+
+//         margin-top: .5rem;
+//         color: $gray;
+
+//         .link{
+//             border-radius: 0.375rem;
+//             @include smalltext;
+//             color: $ligt-gray;
+//             text-decoration: underline;
+            
+//             &:hover{
+//                 color: $semi-black;
+//             }
+
+//             &:focus{
+//                 outline: none;
+//                 box-shadow: 0 0 0 2px $indigo, 0 0 0 4px rgba(255, 255, 255, 1);
+//             }
+//         }
+
+//         .message{
+//             @include smalltext;
+
+//             margin-top: .5rem;
+//             font-weight: 500;
+//             color: rgb(0, 156, 0);
+//         }
+        
+//     }
+
+//     .btn{
+//         display: flex;
+//         align-items: center;
+//         gap: 1rem;
+//         margin-top: 1rem;
+
+//         .message{
+//             @include smalltext;
+
+//             color: $ligt-gray;
+//         }
+//     }
+
+    
+// }
 </style>
