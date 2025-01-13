fe52ccb3c3c91ec7d71425b84c602245a31b8422
jkcoderdev <20jackoza06@gmail.com>
2025-01-13 12:48:30 +0100

    Translate Tailwind CSS classes to SASS in Dashboard.vue

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -7,24 +7,60 @@ import { Head } from '@inertiajs/vue3';
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
-        <template #header>
-            <h2
-                class="text-xl font-semibold leading-tight text-gray-800"
-            >
-                Dashboard
-            </h2>
+        <template #header class="header">
+            <h2>Dashboard</h2>
         </template>
 
-        <div class="py-12">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
-                >
-                    <div class="p-6 text-gray-900">
-                        You're logged in!
-                    </div>
+        <div class="content py-12">
+            <div class="section">
+                <div class="text">
+                    You're logged in!
                 </div>
             </div>
         </div>
     </AuthenticatedLayout>
 </template>
+
+<style lang="scss" scoped>
+.header h2 {
+    font-size: 1.25rem;
+    line-height: 1.25;
+    font-weight: 600;
+    color: #1f2937;
+}
+
+.content {
+    max-width: 80rem;
+
+    padding: 3rem 0;
+    margin: 0 auto;
+
+    @media (min-width: 640px) {
+        padding-left: 1.5rem;
+        padding-right: 1.5rem;
+    }
+
+    @media (min-width: 1024px) {
+        padding-left: 2rem;
+        padding-right: 2rem;
+    }
+
+    .section {
+        overflow: hidden;
+
+        padding: 1.5rem;
+
+        background-color: #ffffff;
+
+        box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+        
+        @media (min-width: 640px) {
+            border-radius: 0.5rem;
+        }
+
+        .text {
+            color: #111827;
+        }
+    }
+}
+</style>
